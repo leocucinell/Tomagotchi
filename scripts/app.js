f093410053402ac5c2230c05eda45cd26cb97ae1
LeoCucinell <leocucinell@gmail.com>
2021-07-14 10:50:12 -0700

    structured the time progression logic for the game

--- scripts/app.js
+++ scripts/app.js
@@ -6,35 +6,42 @@ console.log($);
 */
 const mainGame = {
     userFrog: "Hoppy",
-    eggStage:{
-        progressBars: ["Temperature"],
-        buttons: ["Increase Temperature"],
-        image: "../assets/Frog Egg.png"
-    },
-    tadpoleStage:{
-        progressBars: ["Temperature", "Food"],
-        buttons: ["Increase Temperature", "Feed"],
-        image: "../assets/tadpole.png"
-    },
-    froglettStage:{
-        progressBars: ["Temperature", "Food", "Water Quality"],
-        buttons: ["Increase Temperature", "Feed", "Clean Water"],
-        image: "../assets/froglett.png"
-    },
-    frogStage:{
-        progressBars: ["Temperature", "Food", "Water Quality", "Exercise"],
-        buttons: ["Increase Temperature", "Feed", "Clean Water", "Exercise"],
-        image: "../assets/frog.png"
-    },
-    timer: setInterval(function(){
-        console.log("GAME TIMER")
-    }, 1000)
+    userStage: 1, //I migtht not need this... just use the amount of time passed
+    stages:[
+        {
+            name: "Egg stage",
+            progressBars: ["Temperature"],
+            buttons: ["Increase Temperature"],
+            image: "../assets/Frog Egg.png"
+        },
+        {
+            name: "Tadpole stage",
+            progressBars: ["Temperature", "Food"],
+            buttons: ["Increase Temperature", "Feed"],
+            image: "../assets/tadpole.png"
+        },
+        {
+            name: "Froglett stage",
+            progressBars: ["Temperature", "Food", "Water Quality"],
+            buttons: ["Increase Temperature", "Feed", "Clean Water"],
+            image: "../assets/froglett.png"
+        },
+        {
+            name: "Frog Stage",
+            progressBars: ["Temperature", "Food", "Water Quality", "Exercise"],
+            buttons: ["Increase Temperature", "Feed", "Clean Water", "Exercise"],
+            image: "../assets/frog.png"
+        }
+    ],
+    timer: null,
+    currentTime: 0,
+    isDead: false
 }
 
 /* SECTION: Welcome Screen
     NOTE:
         - first check to see if you can grab the input on user clicking the submit button
-        - Then make the screen disapear
+        - Then make the screen disapear and start the game
 */
 
 $("#frog-submit").on("click", function(){
@@ -45,21 +52,40 @@ $("#frog-submit").on("click", function(){
     $(".welcome-screen").css("display", "none");
     //update the text within the egg screen
     $("#pet-frog").text(mainGame.userFrog);
-    game();
+    //run the game code in an interval
+    mainGame.timer = setInterval(game, 1000);
 });
 
-/* SECTION: Egg Screen
+/* SECTION: The Game
     NOTE:
-        - See if you can display the input name that the user chose from welcome
+        - render a different amount of progress bars and buttons based on the amount of time passed
 */
-function game(){
-    //make a timer that will deplete the progress bar by 10% every second
-    const progressTimer = setInterval(function(){
-        //take the progress timer and subtract 10% off it
-        const currentValue = $(".progress-bar___main").val();
-        $(".progress-bar___main").val(currentValue - 10);
-    }, 1000);
+const game = function(){
+    //update the amount of time that the user has been playing the game
+    mainGame.currentTime++;
+
+    //check to see how much time has passed or if the frog died :-(
+    //based on that time, render out different progress bars
+
+    if(mainGame.isDead){
+        //the frog has passed away because Grogu ate him
+    } else if(mainGame.currentTime <= 30){
+        console.log(mainGame.stages[0]);
+    } else if(mainGame.currentTime > 30 && mainGame.currentTime <= 60){
+        console.log(mainGame.stages[1]);
+    } else if(mainGame.currentTime > 60 && mainGame.currentTime <= 120){
+        console.log(mainGame.stages[2]);
+    } else {
+        console.log(mainGame.stages[3]);
+    }
+
 }
 
-/* SECTION: Game functionality methods */
-$()
+/* SECTION: Render Helper Methods
+    NOTE: This section is to organize the render of the different stages of frog lifecycle
+*/
+function renderStage(currentStage){
+    //append the correct amount of progress bars to the .progress-container
+    //append the appropriate amount of buttons to the .buttons-container
+    
+}
